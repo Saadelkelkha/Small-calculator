d65df6b74fd8aa19d24896b7da9eccf15c52cb09
Saad el kelkha <146880449+Saadelkelkha@users.noreply.github.com>
2023-10-11 09:28:14 +0100

    Add files via upload

---
+++ operation ex20.py
@@ -0,0 +1,13 @@
+A=int(input("type the first number :"))
+B=int(input("type the second number :"))
+OP=input("type the operation :")
+if OP == "+" :
+  print("the result is :", A+B)
+elif OP == "-" :
+  print("the result is :", A-B)
+elif OP == "*" :
+ print("the result is :", A*B)
+elif OP == "/" :
+ print("the result is :",A/B)
+else :
+ print("operator does not exist")
